9c55ebc05fbcb18c326aac0431e47005611c2e88
altescape <hello@michaelwatts.me>
2024-03-08 18:59:42 +0000

    Update meta viewport and sidebar styling

--- src/web-components/toggle-theme.ts
+++ src/web-components/toggle-theme.ts
@@ -1,6 +1,11 @@
+enum ToggleThemeMode {
+  Light = "light",
+  Dark = "dark",
+}
+
 class ToggleTheme extends HTMLElement {
   #shadow: ShadowRoot | null = null;
-  #mode: string = "#light-mode";
+  #mode: string = ToggleThemeMode.Light;
   #html: HTMLElement | null = null;
   #icon: HTMLElement | null | undefined = null;
 
@@ -10,38 +15,51 @@ class ToggleTheme extends HTMLElement {
   }
 
   toggleMode() {
-    if (this.#mode === "#dark-mode") {
-      this.#mode = "#light-mode";
+    if (this.#mode === ToggleThemeMode.Dark) {
+      this.#mode = ToggleThemeMode.Light;
     } else {
-      this.#mode = "#dark-mode";
+      this.#mode = ToggleThemeMode.Dark;
     }
   }
 
   toggleLocalStorage() {
-    if (this.#mode === "#dark-mode") {
-      localStorage.setItem("theme", "dark");
+    if (this.#mode === ToggleThemeMode.Dark) {
+      localStorage.setItem("theme", ToggleThemeMode.Dark);
     } else {
       localStorage.removeItem("theme");
     }
   }
 
   toggleHtmlClass() {
-    if (this.#html === null) return;
-
-    if (this.#mode === "#dark-mode") {
-      this.#html.classList.add("dark");
+    if (this.#mode === ToggleThemeMode.Dark) {
+      this.#html?.classList.add(ToggleThemeMode.Dark);
     } else {
-      this.#html.classList.remove("dark");
+      this.#html?.classList.remove(ToggleThemeMode.Dark);
+    }
+  }
+
+  toggleSelfClass() {
+    console.log("hello", this.#mode);
+    const toggleThemeElement = document.querySelector("toggle-theme");
+
+    const button =
+      toggleThemeElement?.shadowRoot?.getElementById("toggle-theme");
+
+    if (this.#mode === ToggleThemeMode.Dark) {
+      button?.classList.add(ToggleThemeMode.Dark);
+      console.log("hello 1", button?.classList);
+    } else {
+      button?.classList.remove(ToggleThemeMode.Dark);
+      console.log("hello 2", button?.classList);
     }
   }
 
   toggleIcon() {
     const toggleThemeElement = document.querySelector("toggle-theme");
-    if (toggleThemeElement === null) return;
 
     const iconContainer =
-      toggleThemeElement.shadowRoot?.querySelector(".icon-container");
-    this.#icon = toggleThemeElement.shadowRoot?.querySelector("#icon");
+      toggleThemeElement?.shadowRoot?.querySelector(".icon-container");
+    this.#icon = toggleThemeElement?.shadowRoot?.querySelector("#icon");
 
     if (this.#icon === null || this.#icon === undefined) return;
 
@@ -49,7 +67,7 @@ class ToggleTheme extends HTMLElement {
 
     setTimeout(() => {
       if (this.#icon !== null && this.#icon !== undefined) {
-        this.#icon.innerHTML = `<use href="${this.#mode}" />`;
+        this.#icon.innerHTML = `<use href="#${this.#mode}" />`;
       }
     }, 500);
 
@@ -68,21 +86,25 @@ class ToggleTheme extends HTMLElement {
 
     this.#html = document.querySelector("html");
 
-    if (localStorage.getItem("theme") === "dark") {
-      this.#mode = "#dark-mode";
+    if (localStorage.getItem("theme") === ToggleThemeMode.Dark) {
+      this.#mode = ToggleThemeMode.Dark;
     } else {
-      this.#mode = "#light-mode";
+      this.#mode = ToggleThemeMode.Light;
     }
 
     this.toggleHtmlClass();
+    this.toggleSelfClass();
 
     this.#shadow.innerHTML = `
       <style>
         #toggle-theme {
-          --icon-color: #b98714;
-          --icon-color-darker: #b97614;
+          --icon-background-color-dark: #000000;
+          --icon-background-color-light: #44b7e6;
+          --icon-color: #F0E68C;
+          --icon-color-darker: #FFC72C;
         }
         #toggle-theme {
+          background: var(--icon-background-color-light);
           transition: background-color 0.3s ease;
           border-radius: 50%;
           overflow: hidden;
@@ -93,6 +115,9 @@ class ToggleTheme extends HTMLElement {
           width: 26px;
           height: 26px;
           padding: 0;
+        }
+        #toggle-theme.dark {
+          background: var(--icon-background-color-dark);
         }
         #toggle-theme:hover {
           cursor: pointer;
@@ -111,26 +136,18 @@ class ToggleTheme extends HTMLElement {
         .icon-container.done-animating {
           animation: none;
         }
-        @keyframes rotate {
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(-90deg);
-          }
-        }
         @keyframes move {
           0% {
-            transform: translateX(0) rotate(0deg);
+            transform: translateX(0);
           }
           50% {
-            transform: translateX(100%) rotate(0deg);
+            transform: translateX(100%);
           }
           50.001% {
-            transform: translateX(-35%) rotate(0deg) scale(0.2);
+            transform: translateX(-35%) scale(0.2);
           }
           100% {
-            transform: translateX(0) rotate(360deg) scale(1);
+            transform: translateX(0) scale(1);
           }
         }
         .icon {
@@ -139,10 +156,11 @@ class ToggleTheme extends HTMLElement {
           transition: fill 0.3s ease;
           fill: var(--icon-color);
         }
-        #toggle-theme:hover .icon {
+        #toggle-theme.dark .icon {
           fill: var(--icon-color-darker);
-          animation: rotate 1s infinite linear;
-          animation-direction: alternate;
+        }
+        #toggle-theme:hover {
+          background-filter: brightness(1.2);
         }
         .hidden {
           display: none;
@@ -151,20 +169,21 @@ class ToggleTheme extends HTMLElement {
       <button
         id="toggle-theme"
         aria-label="Toggle light/dark mode"
+        class="${this.#mode}"
       >
         <div class="icon-container">
           <svg id="icon" class="icon">
-            <use href="${this.#mode}" />
+            <use href="#${this.#mode}" />
           </svg>
         </div>
       </button>
       <svg xmlns="http://www.w3.org/2000/svg" class="hidden">
-        <symbol id="light-mode" viewBox="0 0 16 16">
+        <symbol id="light" viewBox="0 0 16 16">
           <path
             d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"
           />
         </symbol>
-        <symbol id="dark-mode" viewBox="0 0 16 16">
+        <symbol id="dark" viewBox="0 0 16 16">
           <path
             d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"
           />
@@ -176,6 +195,7 @@ class ToggleTheme extends HTMLElement {
       this.toggleMode();
       this.toggleLocalStorage();
       this.toggleHtmlClass();
+      this.toggleSelfClass();
       this.toggleIcon();
     });
   }
